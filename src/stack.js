8181101546affb10f4e7b3128cb23a8dbf3f0f28
VitaliMay <mvt30@mail.ru>
2023-04-16 21:24:43 +0300

    init: check how it works

--- src/stack.js
+++ src/stack.js
@@ -14,7 +14,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 class Stack {
   push(/* element */) {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
